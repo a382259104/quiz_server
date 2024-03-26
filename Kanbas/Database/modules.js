21b6be720889e75d05e70b1cb8a1f420ca62439c
YuFeng Lin <a382259104@gmail.com>
2024-03-26 15:26:43 -0400

    last commit before going live

---
+++ Kanbas/Database/modules.js
@@ -0,0 +1,470 @@
+export default[
+    {
+      "_id": "M101",
+      "name": "Introduction to Rocket Propulsion",
+      "description": "Basic principles of rocket propulsion and rocket engines.",
+      "course": "RS101",
+      "lessons": [
+        {
+          "_id": "L101",
+          "name": "History of Rocketry",
+          "description": "A brief history of rocketry and space exploration.",
+          "module": "M101"
+        },
+        {
+          "_id": "L102",
+          "name": "Rocket Propulsion Fundamentals",
+          "description": "Basic principles of rocket propulsion.",
+          "module": "M101"
+        },
+        {
+          "_id": "L103",
+          "name": "Rocket Engine Types",
+          "description": "Overview of different types of rocket engines.",
+          "module": "M101"
+        }
+      ]
+    },
+    {
+      "_id": "M102",
+      "name": "Fuel and Combustion",
+      "description": "Understanding rocket fuel, combustion processes, and efficiency.",
+      "course": "RS101",
+      "lessons": [
+        {
+          "_id": "L201",
+          "name": "Rocket Fuel",
+          "description": "Overview of different types of rocket fuels.",
+          "module": "M102"
+        },
+        {
+          "_id": "L202",
+          "name": "Combustion Processes",
+          "description": "Understanding combustion processes and efficiency.",
+          "module": "M102"
+        },
+        {
+          "_id": "L203",
+          "name": "Combustion Instability",
+          "description": "Understanding combustion instability and mitigation.",
+          "module": "M102"
+        }
+      ]
+    },
+    {
+      "_id": "M103",
+      "name": "Nozzle Design",
+      "description": "Principles of rocket nozzle design and performance optimization.",
+      "course": "RS101",
+      "lessons": [
+        {
+          "_id": "L301",
+          "name": "Nozzle Design",
+          "description": "Overview of different types of rocket nozzles.",
+          "module": "M103"
+        },
+        {
+          "_id": "L302",
+          "name": "Nozzle Performance",
+          "description": "Understanding nozzle performance and efficiency.",
+          "module": "M103"
+        },
+        {
+          "_id": "L303",
+          "name": "Nozzle Optimization",
+          "description": "Optimizing nozzle design for specific applications.",
+          "module": "M103"
+        }
+      ]
+    },
+    {
+      "_id": "M201",
+      "name": "Fundamentals of Aerodynamics",
+      "description": "Basic aerodynamic concepts and fluid dynamics principles.",
+      "course": "RS102",
+      "lessons": [
+        {
+          "_id": "L101",
+          "name": "Intro to Physics",
+          "description": "How universe started",
+          "module": "M101"
+        },
+        {
+          "_id": "L102",
+          "name": "?????",
+          "description": "????????????????",
+          "module": "M101"
+        },
+        {
+          "_id": "L103",
+          "name": "???????????",
+          "description": "????????????????",
+          "module": "M101"
+        }
+      ]
+    },
+    {
+      "_id": "M202",
+      "name": "Subsonic and Supersonic Flow",
+      "description": "Understanding subsonic and supersonic aerodynamic behaviors.",
+      "course": "RS102",
+      "lessons": [
+        {
+          "_id": "L101",
+          "name": "Intro to Physics",
+          "description": "How universe started",
+          "module": "M101"
+        },
+        {
+          "_id": "L102",
+          "name": "?????",
+          "description": "????????????????",
+          "module": "M101"
+        },
+        {
+          "_id": "L103",
+          "name": "???????????",
+          "description": "????????????????",
+          "module": "M101"
+        }
+      ]
+    },
+    {
+      "_id": "M203",
+      "name": "Aerodynamic Heating",
+      "description": "Study of aerodynamic heating and thermal protection systems.",
+      "course": "RS102",
+      "lessons": [
+        {
+          "_id": "L101",
+          "name": "Intro to Physics",
+          "description": "How universe started",
+          "module": "M101"
+        },
+        {
+          "_id": "L102",
+          "name": "?????",
+          "description": "????????????????",
+          "module": "M101"
+        },
+        {
+          "_id": "L103",
+          "name": "???????????",
+          "description": "????????????????",
+          "module": "M101"
+        }
+      ]
+    },
+    {
+      "_id": "M301",
+      "name": "Spacecraft Structural Design",
+      "description": "Fundamentals of designing spacecraft structures and materials selection.",
+      "course": "RS103",
+      "lessons": [
+        {
+          "_id": "L101",
+          "name": "Intro to Physics",
+          "description": "How universe started",
+          "module": "M101"
+        },
+        {
+          "_id": "L102",
+          "name": "?????",
+          "description": "????????????????",
+          "module": "M101"
+        },
+        {
+          "_id": "L103",
+          "name": "???????????",
+          "description": "????????????????",
+          "module": "M101"
+        }
+      ]
+    },
+    {
+      "_id": "M302",
+      "name": "Orbital Mechanics",
+      "description": "Understanding orbital dynamics and mission planning.",
+      "course": "RS103",
+      "lessons": [
+        {
+          "_id": "L101",
+          "name": "Intro to Physics",
+          "description": "How universe started",
+          "module": "M101"
+        },
+        {
+          "_id": "L102",
+          "name": "?????",
+          "description": "????????????????",
+          "module": "M101"
+        },
+        {
+          "_id": "L103",
+          "name": "???????????",
+          "description": "????????????????",
+          "module": "M101"
+        }
+      ]
+    },
+    {
+      "_id": "M303",
+      "name": "Spacecraft Systems Engineering",
+      "description": "Overview of spacecraft systems and subsystems engineering.",
+      "course": "RS103",
+      "lessons": [
+        {
+          "_id": "L101",
+          "name": "Intro to Physics",
+          "description": "How universe started",
+          "module": "M101"
+        },
+        {
+          "_id": "L102",
+          "name": "?????",
+          "description": "????????????????",
+          "module": "M101"
+        },
+        {
+          "_id": "L103",
+          "name": "???????????",
+          "description": "????????????????",
+          "module": "M101"
+        }
+      ]
+    },
+    {
+      "_id": "M404",
+      "name": "Introduction to Dr. Dracket",
+      "description": "Why isn't this being rendered anywhere? What's the purpose of this data?",
+      "course": "CS101",
+      "lessons": [
+        {
+          "_id": "L101",
+          "name": "Recursion",
+          "description": "A brief history of rocketry and space exploration.",
+          "module": "M101"
+        },
+        {
+          "_id": "L102",
+          "name": "Functional Language",
+          "description": "Basic principles of rocket propulsion.",
+          "module": "M101"
+        },
+        {
+          "_id": "L103",
+          "name": "Turture",
+          "description": "Overview of different types of rocket engines.",
+          "module": "M101"
+        }
+      ]
+    },
+    {
+      "_id": "M405",
+      "name": "Data Structures!",
+      "description": "Why isn't this being rendered anywhere? What's the purpose of this data?",
+      "course": "CS101",
+      "lessons": [
+        {
+          "_id": "L101",
+          "name": "Uhhhh ConsLo?",
+          "description": "A brief history of rocketry and space exploration.",
+          "module": "M101"
+        },
+        {
+          "_id": "L102",
+          "name": "MT Lo?",
+          "description": "Basic principles of rocket propulsion.",
+          "module": "M101"
+        },
+        {
+          "_id": "L103",
+          "name": "Poop",
+          "description": "Overview of different types of rocket engines.",
+          "module": "M101"
+        }
+      ]
+    },
+    {
+      "_id": "M406",
+      "name": "Blockchain in Racket",
+      "description": "Why isn't this being rendered anywhere? What's the purpose of this data?",
+      "course": "CS101",
+      "lessons": [
+        {
+          "_id": "L101",
+          "name": "What is a blockchain",
+          "description": "A brief history of rocketry and space exploration.",
+          "module": "M101"
+        },
+        {
+          "_id": "L102",
+          "name": "What is a public key",
+          "description": "Basic principles of rocket propulsion.",
+          "module": "M101"
+        },
+        {
+          "_id": "L103",
+          "name": "How to encode messages?",
+          "description": "Overview of different types of rocket engines.",
+          "module": "M101"
+        }
+      ]
+    },
+    {
+      "_id": "M504",
+      "name": "Contemporary Issues",
+      "description": "Why isn't this being rendered anywhere? What's the purpose of this data?",
+      "course": "IN501",
+      "lessons": [
+        {
+          "_id": "L101",
+          "name": "China",
+          "description": "A brief history of rocketry and space exploration.",
+          "module": "M101"
+        },
+        {
+          "_id": "L102",
+          "name": "US",
+          "description": "Basic principles of rocket propulsion.",
+          "module": "M101"
+        },
+        {
+          "_id": "L103",
+          "name": "Pianist",
+          "description": "Overview of different types of rocket engines.",
+          "module": "M101"
+        }
+      ]
+    },
+    {
+      "_id": "M505",
+      "name": "Enduring Issues",
+      "description": "Why isn't this being rendered anywhere? What's the purpose of this data?",
+      "course": "IN501",
+      "lessons": [
+        {
+          "_id": "L101",
+          "name": "Gaza",
+          "description": "A brief history of rocketry and space exploration.",
+          "module": "M101"
+        },
+        {
+          "_id": "L102",
+          "name": "Taiwan",
+          "description": "Basic principles of rocket propulsion.",
+          "module": "M101"
+        },
+        {
+          "_id": "L103",
+          "name": "Ukraine",
+          "description": "Overview of different types of rocket engines.",
+          "module": "M101"
+        }
+      ]
+    },
+    {
+      "_id": "M506",
+      "name": "Old Stuff",
+      "description": "Why isn't this being rendered anywhere? What's the purpose of this data?",
+      "course": "IN501",
+      "lessons": [
+        {
+          "_id": "L101",
+          "name": "WW2 and US",
+          "description": "A brief history of rocketry and space exploration.",
+          "module": "M101"
+        },
+        {
+          "_id": "L102",
+          "name": "Cold War",
+          "description": "Basic principles of rocket propulsion.",
+          "module": "M101"
+        },
+        {
+          "_id": "L103",
+          "name": "Post Cold War",
+          "description": "Overview of different types of rocket engines.",
+          "module": "M101"
+        }
+      ]
+    },
+    {
+      "_id": "M606",
+      "name": "Intro to Networks",
+      "description": "Why isn't this being rendered anywhere? What's the purpose of this data?",
+      "course": "CS301",
+      "lessons": [
+        {
+          "_id": "L101",
+          "name": "How old humans communicated",
+          "description": "A brief history of rocketry and space exploration.",
+          "module": "M101"
+        },
+        {
+          "_id": "L102",
+          "name": "How we invented internet",
+          "description": "Basic principles of rocket propulsion.",
+          "module": "M101"
+        },
+        {
+          "_id": "L103",
+          "name": "Where we are going",
+          "description": "Overview of different types of rocket engines.",
+          "module": "M101"
+        }
+      ]
+    },
+    {
+      "_id": "M505",
+      "name": "History of Modern Internet",
+      "description": "Why isn't this being rendered anywhere? What's the purpose of this data?",
+      "course": "CS301",
+      "lessons": [
+        {
+          "_id": "L101",
+          "name": "Ipv4",
+          "description": "A brief history of rocketry and space exploration.",
+          "module": "M101"
+        },
+        {
+          "_id": "L102",
+          "name": "Facebook broke down",
+          "description": "Basic principles of rocket propulsion.",
+          "module": "M101"
+        },
+        {
+          "_id": "L103",
+          "name": "Security and Monetary Incentives",
+          "description": "Overview of different types of rocket engines.",
+          "module": "M101"
+        }
+      ]
+    },
+    {
+      "_id": "M506",
+      "name": "Layers",
+      "description": "Why isn't this being rendered anywhere? What's the purpose of this data?",
+      "course": "CS301",
+      "lessons": [
+        {
+          "_id": "L101",
+          "name": "How physical signals are compacted into deliverable packets",
+          "description": "A brief history of rocketry and space exploration.",
+          "module": "M101"
+        },
+        {
+          "_id": "L102",
+          "name": "How low level code can be delivered across the world",
+          "description": "Basic principles of rocket propulsion.",
+          "module": "M101"
+        },
+        {
+          "_id": "L103",
+          "name": "How to retrieve the code after you get them",
+          "description": "Overview of different types of rocket engines.",
+          "module": "M101"
+        }
+      ]
+    }
+  ]
