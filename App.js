7318f98f663ffc26d1dc618a71e3e3393eb8ec70
YuFeng Lin <a382259104@gmail.com>
2024-04-04 15:07:37 -0400

    idk if it works

--- App.js
+++ App.js
@@ -1,14 +1,44 @@
+import "dotenv/config.js";
 import express from "express";
+import session from "express-session";
 import Lab5 from "./Lab5.js";
 import cors from "cors";
 import Hello from "./Hello.js";
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
+import mongoose from "mongoose";
+import UserRoutes from "./Users/routes.js";
+
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+mongoose.connect(CONNECTION_STRING);
+
 
 const app = express()
-app.use(cors())
+app.use(cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+}));
+
+const sessionOptions = {
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+};
+
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+    };
+}
+
+app.use(session(sessionOptions));
+
+
 app.use(express.json())
 
+UserRoutes(app)
 Hello(app)
 CourseRoutes(app)
 ModuleRoutes(app)
